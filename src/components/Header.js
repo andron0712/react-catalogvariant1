2c11bf0de7d2223cbf46f1548608e5114d295419
andron0712 <andriygryban07@gmail.com>
2026-06-01 13:56:53 +0300

    Add Header component with styled header

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <header style={{ backgroundColor: '#1a1a1a', padding: '20px', color: 'white', textAlign: 'center' }}>
+      <h1>Мій React Каталог</h1>
+    </header>
+  );
+}
+
+export default Header;
