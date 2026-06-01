f84256499c45e376e7e6b2b117369d8efbc58903
andron0712 <andriygryban07@gmail.com>
2026-06-01 13:58:20 +0300

    Create Footer.js

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer style={{ backgroundColor: '#f8f9fa', padding: '15px', textAlign: 'center', marginTop: 'auto' }}>
+      <p>© 2026 Практична робота. Варіант 1.</p>
+    </footer>
+  );
+}
+
+export default Footer;
